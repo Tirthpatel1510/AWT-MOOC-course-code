5e8502ac1dc2ca346aea50e8ddf2506faeb91f2d
Tirth Patel <175490452+Tirthpatel1510@users.noreply.github.com>
2026-04-20 22:23:20 +0530

    inventory
    upadted

---
+++ inventory.api/src/utils/errors.js
@@ -0,0 +1,19 @@
+function errorHandler(err, req, res, next) {
+  switch (true) {
+    case typeof err === "string":
+      // custom application error
+      const is404 = err.toLowerCase().endsWith("not found");
+      const statusCode = is404 ? 404 : 400;
+      return res.status(statusCode).json({ message: err });
+      break;
+    case err.name === "ValidationError":
+      res.status(400).json({ error: err});
+      break;
+    default:
+      return res.status(500).json({ message: err.message });
+  }
+}
+
+module.exports = {
+  errorHandler,
+};
